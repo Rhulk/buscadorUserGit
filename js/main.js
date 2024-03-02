3fbc54a457910202f570a7c7658bf6ef56e2a144
rhulk <quique1904@gmail.com>
2024-03-02 19:10:55 +0100

    Controlar el tiempo entre peticiones get

--- js/main.js
+++ js/main.js
@@ -1,79 +1,88 @@
 const API = "https://api.github.com/users/";
 
-function debug(){
-    console.log("debug");
-}
-debug();
+const requestMaxTimeMs = 30000
 
 const app = Vue.createApp({
-    data() {
-      return {
-        search: null,
-        message: "Hello Vue!",
-        count: 0,
-        result: null,
-        error: null,
-        favorites: new Map()
-      };
+  data() {
+    return {
+      search: null,
+      result: null,
+      error: null,
+      favorites: new Map()
+    };
+  },
+  created() {
+    const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
+    if (savedFavorites?.length) {
+      const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
+      this.favorites = favorites
+    }
+  },
+  computed: {
+    isFavorite() {
+      return this.favorites.has(this.result.login)
     },
-    created() {
-      const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
-      if (savedFavorites?.length) {
-        const favorites = new Map(savedFavorites.map(favorite => [favorite.id, favorite]))
-        this.favorites = favorites
-      }
-    },
-    computed:{
-      isFavorite() {
-        return this.favorites.has(this.result.id)
-      },
-      allFavorites() {
-        return Array.from(this.favorites.values())
-      }
-    },
+    allFavorites() {
+      return Array.from(this.favorites.values())
+    }
+  },
+  methods: {
+    async doSearch() {
+      this.result = this.error = null
 
-    methods: {
-        async  doSearch() {
-            this.result = this.error = null
-            try{
-                const response = await fetch(API + this.search)
-                if (!response.ok) throw new Error("User not found");
-                const data = await response.json()
-                console.log(data);
-                this.result = data
-                console.log(this.result)
-            }catch(error){
-                console.log(" -KO-"+error)
-                this.error = error;
-            }finally{
-                this.search = null;
-            }
-        },
-        addFavorite() {
-          this.favorites.set(this.result.id, this.result)
-          this.updateStorage();
-        },
-        removeFavorite() {
-          this.favorites.delete(this.result.id)
-          this.updateStorage();
-        },
-        updateStorage() {
-          window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
-        },
-        showFavorite(favorite){
-          this.result = favorite
-        },
-        increment() {
-          this.count++
+      const foundInFavorites = this.favorites.get(this.search)
+
+      const shouldRequestAgain = (() => {
+        if (!!foundInFavorites) {
+          const { lastRequestTime } = foundInFavorites
+          const now = Date.now()
+          return (now - lastRequestTime) > requestMaxTimeMs
         }
-      },
-      mounted() {
-        // methods can be called in lifecycle hooks, or other methods!
-        this.increment()
+        return false
+      })() // IIFE
+
+      if (!!foundInFavorites && !shouldRequestAgain) {
+        console.log("Found and we use the cached version")
+        return this.result = foundInFavorites
       }
+
+      try {
+        console.log("Not found or cached version is too old")
+        const response = await fetch(API + this.search)
+        if (!response.ok) throw new Error("User not found")
+        const data = await response.json()
+        console.log(data)
+        this.result = data
+        if (foundInFavorites){
+            foundInFavorites.lastRequestTime = Date.now()    
+        }
+        
+      } catch(error) {
+        this.error = error
+      } finally {
+        this.search = null
+      }
+    },
+
+    addFavorite() {
+      this.result.lastRequestTime = Date.now()
+      this.favorites.set(this.result.login, this.result)
+      this.updateStorage()
+    },
+
+    removeFavorite() {
+      this.favorites.delete(this.result.login)
+      this.updateStorage()
+    },
+
+    showFavorite(favorite) {
+      this.result = favorite
+    },
+
+    updateStorage() {
+      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
+    }
   }
-  
-  
-  );
+});
 
-
+const mountedApp = app.mount("#app")
